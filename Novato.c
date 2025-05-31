ab42730313e763463e5dff37ad6cbb181155021f
Paulada-S <01jpaulo10@gmail.com>
2025-05-31 01:48:09 +0000

    Criação de Super Trunfo

---
+++ Novato.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main () {
+
+int populacao, pontos_turisticos;
+float area, PIB;
+
+printf("Digite o numero populacional: \n");
+scanf("%d", &populacao);
+
+printf("O numero de pontos turisticos é: \n");
+printf("%d", &pontos_turisticos);
+
+printf("A area é: \n");
+scanf("%f", &area);
+
+printf("O PIB é: \n");
+scanf("%f", &PIB);
+
+printf("O numero populacional é: %d\n", &populacao);
+printf("O número de pontos turisticos é: %d\n", &pontos_turisticos);
+printf("A área é: %f\n", &area);
+printf("O PIB é de: %f\n", &PIB)
+
+
+}sssaddn b
