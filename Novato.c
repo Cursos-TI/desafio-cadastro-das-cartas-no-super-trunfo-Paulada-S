81e167b25dfb0b1b675f09e12addb700cf18c85c
Paulada-S <01jpaulo10@gmail.com>
2025-06-03 02:21:48 +0000

    S.T. Mestre

--- Novato.c
+++ Novato.c
@@ -2,25 +2,83 @@
 
 int main () {
 
-int populacao, pontos_turisticos;
-float area, PIB;
+float populacao, area, PIB, turisticos;
+float densidade, per_capita;
+char codigo[20];
+char estado[20];
+char cidade[20];
 
-printf("Digite o numero populacional: \n");
-scanf("%d", &populacao);
+densidade = (populacao / area);
+per_capita = (PIB / populacao);
 
-printf("O numero de pontos turisticos é: \n");
-printf("%d", &pontos_turisticos);
+printf("Digite o código do estado: \n");
+scanf("%s", &codigo);
 
-printf("A area é: \n");
+printf("Digite o nome do estado: \n");
+scanf("%s", &estado);
+
+printf("Digite o nome da cidade: \n");
+scanf("%s", &cidade);
+
+printf("Digite a área do estado: \n");
 scanf("%f", &area);
 
-printf("O PIB é: \n");
+printf("Digite o número populacional: \n");
+scanf("%f", &populacao);
+
+printf("Digite o PIB: \n");
 scanf("%f", &PIB);
 
-printf("O numero populacional é: %d\n", &populacao);
-printf("O número de pontos turisticos é: %d\n", &pontos_turisticos);
-printf("A área é: %f\n", &area);
-printf("O PIB é de: %f\n", &PIB);
+printf("Digite o número de pontos turísticos: \n");
+scanf("%f", &turisticos);
 
+densidade = (populacao / area);
+per_capita = (PIB / populacao);
+
+printf("Dados da carta 1 \n");
+printf("O código do estado é: %s\n", codigo);
+printf("O nome do estado é: %s\n", estado);
+printf("O nome da cidade é: %s\n", cidade);
+printf("A área do estado é: %2.f\n", area);
+printf("O o número populacional é: %2.f\n", populacao);
+printf("O PIB é: %2.f\n", PIB);
+printf("O número de pontos turísticos é: %2.f\n", turisticos);
+printf("A densidade é: %2.f\n", densidade);
+printf("O PIB per capita é: %2.f\n", per_capita);
+
+printf("Digite o código do estado: \n");
+scanf("%s", &codigo);
+
+printf("Digite o nome do estado: \n");
+scanf("%s", &estado);
+
+printf("Digite o nome da cidade: \n");
+scanf("%s", &cidade);
+
+printf("Digite a área do estado: \n");
+scanf("%f", &area);
+
+printf("Digite o número populacional: \n");
+scanf("%f", &populacao);
+
+printf("Digite o PIB: \n");
+scanf("%f", &PIB);
+
+printf("Digite o número de pontos turísticos: \n");
+scanf("%f", &turisticos);
+
+densidade = (populacao / area);
+per_capita = (PIB / populacao);
+
+printf("Dados da carta 2 \n");
+printf("O código do estado é: %s\n", codigo);
+printf("O nome do estado é: %s\n", estado);
+printf("O nome da cidade é: %s\n", cidade);
+printf("A área do estado é: %2.f\n", area);
+printf("O o número populacional é: %2.f\n", populacao);
+printf("O PIB é: %2.f\n", PIB);
+printf("O número de pontos turísticos é: %2.f\n", turisticos);
+printf("A densidade é: %2.f\n", densidade);
+printf("O PIB per capita é: %2.f\n", per_capita);
 
 }
